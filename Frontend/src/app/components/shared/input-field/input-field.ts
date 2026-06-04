02d13fb7747bcdb4ec23547bf5c69d943e1cb201
Sleiman <sleimanelhajj7@gmail.com>
2026-06-04 20:59:23 +0300

    fixed the header and date

--- Frontend/src/app/components/shared/input-field/input-field.ts
+++ Frontend/src/app/components/shared/input-field/input-field.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './input-field.html',
   styleUrl: './input-field.css',
 })
